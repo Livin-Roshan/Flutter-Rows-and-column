f8e66eb7b822371f7b665b7e8cc330f08c1fa3e8
Livin-Roshan <livinroshan@gmail.com>
2023-06-30 12:18:18 +0530

    This App is provided with column and rows widgets in it.

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,119 @@
 import 'package:flutter/material.dart';
-import 'home.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Column and Rows',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHome(),
+      home: Scaffold(
+
+        appBar: AppBar(
+          title: Text("Flutter Rows and Column"),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: <Widget> [
+
+            Expanded(
+              flex: 2,
+              child: Container(
+                color: Color(0xFF2AA650),
+                alignment: Alignment.center,
+                child: Text("#2AA650",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.white,
+                  ),
+                ),
+
+
+              ),
+            ),
+
+
+            Expanded(
+              flex: 2,
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    flex: 3,
+                    child: Container(
+                      color: Color(0xFF58AAE8),
+                      alignment: Alignment.center,
+                      child: Text("#58AAE8",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 25,
+                          color: Colors.white,
+                        ),
+
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 7,
+                    child: Container(
+                      color: Color(0xFFE74E33),
+                      alignment: Alignment.center,
+                      child: Text("#E74E33",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 25,
+                          color: Colors.white,
+                        ),
+
+                      ),
+                    ),
+                  )
+
+                ],
+              )
+            ),
+
+            Expanded(
+              flex: 5,
+              child: Container(
+                color: Color(0xFF8D43B3),
+                alignment: Alignment.center,
+                child: Text("#8D43B3",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.white,
+                  ),
+
+                ),
+              ),
+            ),
+
+
+            Expanded(
+              flex: 1,
+              child: Container(
+                color: Color(0xFF58AAE8),
+                alignment: Alignment.center,
+                child: Text("#58AAE8",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.white,
+                  ),
+
+                ),
+              ),
+            ),
+
+          ],
+        ),
+      )
     );
   }
 }
+
